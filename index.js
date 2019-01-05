48d9d0604c0d59eeb6ab8dc3e55315f1fbf63a2d
John Mark Redding <johnmarkredding@gmail.com>
2019-01-05 23:09:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  drivers.filter(elm, function (elm, string) {return elm.toLowerCase() === string.toLowerCase()})
+  return drivers.filter(elm, function (elm, string) {
+    return elm.toLowerCase() === string.toLowerCase()
+
+  });
 }
